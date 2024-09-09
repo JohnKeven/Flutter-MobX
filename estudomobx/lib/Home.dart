6e89d6701ff9d247fd2ad6407489e97309102fc5
John Keven <johnkeven.ti@gmail.com>
2024-09-09 16:40:46 -0300

    Update Home.dart

--- estudomobx/lib/Home.dart
+++ estudomobx/lib/Home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            //  Padding(
+            //  Padding( 
             //   padding: const EdgeInsets.all(16),
             //   child: Observer(
             //     builder: (_) => Text(
